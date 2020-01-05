4cb41220a819a0b7fae576c398c9a2a0fcac1769
Nicola Di Trani <nicola.ditrani@gmail.com>
2020-01-05 23:01:55 +0100

    check mosse invalidanti

--- src/app/sudoku.component.ts
+++ src/app/sudoku.component.ts
@@ -30,11 +30,14 @@ export class Sudoku implements OnInit {
     let checkSum = list && list.length > 0 ? list.reduce((a,b)=>a+b) : 0;
 
     this.checkEmpty();
-
     this.checkRows();
     this.checkCols();
     this.checkBlocks();
+    
+    this.checkMosseInvalidanti();
 
+    return this.matrixToList().filter(e=>!!e.wrongNumber).length === 0;
+    
   }
 
   solveByStep(){
@@ -108,11 +111,16 @@ export class Sudoku implements OnInit {
                 //this.solve();
               }
               else{
-                //e.value=n;
-                //this.cellChange(e);
+                e.value=n;
+                this.cellChange(e);
                 if(options.autoSolve) this.solve();
-                //Condizione di stop del ForEach
-                throw {};
+                if(this.checkSudoku()){
+                  //Condizione di stop del ForEach
+                  throw {};  
+                }else{
+                  //Se la mossa corrente ha invalidato il sudoku annullala
+                  this.undo();
+                }
               }
             }
 
@@ -198,7 +206,7 @@ export class Sudoku implements OnInit {
     for(let r=1; r<=9; r++){
       let sublist = list.filter(e=>e.r===r && !!e.value);
       sublist.forEach(n=>{
-        if(sublist.filter(e=>e.value===n.value).length>1)
+        if(sublist.filter(e=>e.value===n.value || e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
           n.wrongNumber=true;
       });
     }
@@ -208,7 +216,7 @@ export class Sudoku implements OnInit {
     for(let c=1; c<=9; c++){
       let sublist = list.filter(e=>e.c===c && !!e.value);
       sublist.forEach(n=>{
-        if(sublist.filter(e=>e.value===n.value).length>1)
+        if(sublist.filter(e=>e.value===n.value || e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
           n.wrongNumber=true;
       });
     }
@@ -221,7 +229,36 @@ export class Sudoku implements OnInit {
         let sublist = list.filter(e=>e.r>=minR && e.r<=maxR && !!e.value);
         sublist = sublist.filter(e=>e.c>=minC && e.c<=maxC && !!e.value);
         sublist.forEach(n=>{
-          if(sublist.filter(e=>e.value===n.value).length>1)
+          if(sublist.filter(e=>e.value===n.value || e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
+            n.wrongNumber=true;
+        });
+      } 
+    }
+  }
+
+  checkMosseInvalidanti(){
+    let list = this.matrixToList();
+    for(let r=1; r<=9; r++){
+      let sublist = list.filter(e=>e.r===r && !e.value);
+      sublist.forEach(n=>{
+        if(sublist.filter(e=>e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
+          n.wrongNumber=true;
+      });
+    }
+    for(let c=1; c<=9; c++){
+      let sublist = list.filter(e=>e.c===c && !e.value);
+      sublist.forEach(n=>{
+        if(sublist.filter(e=>e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
+          n.wrongNumber=true;
+      });
+    }
+    for(let br=0; br<=2; br++){
+      for(let bc=0; bc<=2; bc++){
+        let minR=br*3+1,maxR=br*3+3,minC=bc*3+1,maxC=bc*3+3;
+        let sublist = list.filter(e=>e.r>=minR && e.r<=maxR && !e.value);
+        sublist = sublist.filter(e=>e.c>=minC && e.c<=maxC && !e.value);
+        sublist.forEach(n=>{
+          if(sublist.filter(e=>e.numbers.length === 1 && this.isEqual(e.numbers,n.numbers)).length>1)
             n.wrongNumber=true;
         });
       } 
@@ -372,10 +409,6 @@ export class Sudoku implements OnInit {
         this.cellChange({r:r+1,c:c+1,value:v,fixed:true});
       });
     })
-  }
-  mockSchema2(){
-    let mockString = '[{"r":1,"c":1,"numbers":[2,4]},{"r":1,"c":2,"value":6,"numbers":[],"fixed":true},{"r":1,"c":3,"numbers":[2,7]},{"r":1,"c":4,"value":3,"numbers":[],"fixed":true},{"r":1,"c":5,"numbers":[],"value":9},{"r":1,"c":6,"numbers":[4,5]},{"r":1,"c":7,"value":8,"numbers":[],"fixed":true},{"r":1,"c":8,"numbers":[4,5,7]},{"r":1,"c":9,"value":1,"numbers":[],"fixed":true},{"r":2,"c":1,"numbers":[],"value":1},{"r":2,"c":2,"numbers":[],"value":9},{"r":2,"c":3,"numbers":[],"value":8},{"r":2,"c":4,"numbers":[4,5]},{"r":2,"c":5,"value":7,"numbers":[],"fixed":true},{"r":2,"c":6,"value":2,"numbers":[],"fixed":true},{"r":2,"c":7,"value":3,"numbers":[],"fixed":true},{"r":2,"c":8,"numbers":[4,5,6]},{"r":2,"c":9,"numbers":[4,6]},{"r":3,"c":1,"numbers":[3,4]},{"r":3,"c":2,"value":5,"numbers":[],"fixed":true},{"r":3,"c":3,"numbers":[3,7]},{"r":3,"c":4,"value":8,"numbers":[],"fixed":true},{"r":3,"c":5,"value":1,"numbers":[],"fixed":true},{"r":3,"c":6,"numbers":[],"value":6},{"r":3,"c":7,"numbers":[],"value":2},{"r":3,"c":8,"numbers":[4,7]},{"r":3,"c":9,"value":9,"numbers":[],"fixed":true},{"r":4,"c":1,"numbers":[2,3]},{"r":4,"c":2,"value":8,"numbers":[],"fixed":true},{"r":4,"c":3,"numbers":[2,3]},{"r":4,"c":4,"value":9,"numbers":[],"fixed":true},{"r":4,"c":5,"value":5,"numbers":[],"fixed":true},{"r":4,"c":6,"numbers":[],"value":7},{"r":4,"c":7,"numbers":[4,6]},{"r":4,"c":8,"numbers":[],"value":1},{"r":4,"c":9,"numbers":[4,6]},{"r":5,"c":1,"numbers":[],"value":6},{"r":5,"c":2,"value":7,"numbers":[],"fixed":true},{"r":5,"c":3,"value":9,"numbers":[],"fixed":true},{"r":5,"c":4,"numbers":[],"value":1},{"r":5,"c":5,"numbers":[],"value":4},{"r":5,"c":6,"value":8,"numbers":[],"fixed":true},{"r":5,"c":7,"value":5,"numbers":[],"fixed":true},{"r":5,"c":8,"value":2,"numbers":[],"fixed":true},{"r":5,"c":9,"value":3,"numbers":[],"fixed":true},{"r":6,"c":1,"value":5,"numbers":[],"fixed":true},{"r":6,"c":2,"numbers":[],"value":4},{"r":6,"c":3,"value":1,"numbers":[],"fixed":true},{"r":6,"c":4,"value":2,"numbers":[],"fixed":true},{"r":6,"c":5,"value":6,"numbers":[],"fixed":true},{"r":6,"c":6,"numbers":[],"value":3},{"r":6,"c":7,"value":7,"numbers":[],"fixed":true},{"r":6,"c":8,"numbers":[],"value":9},{"r":6,"c":9,"value":8,"numbers":[],"fixed":true},{"r":7,"c":1,"numbers":[],"value":7},{"r":7,"c":2,"value":2,"numbers":[],"fixed":true},{"r":7,"c":3,"value":4,"numbers":[],"fixed":true},{"r":7,"c":4,"numbers":[],"value":6},{"r":7,"c":5,"numbers":[],"value":8,"fixed":true},{"r":7,"c":6,"numbers":[],"value":9},{"r":7,"c":7,"numbers":[],"value":1,"fixed":true},{"r":7,"c":8,"numbers":[],"value":3,"fixed":true},{"r":7,"c":9,"numbers":[],"value":5},{"r":8,"c":1,"numbers":[],"value":9,"fixed":true},{"r":8,"c":2,"numbers":[],"value":1},{"r":8,"c":3,"numbers":[5,6]},{"r":8,"c":4,"numbers":[],"value":7,"fixed":true},{"r":8,"c":5,"numbers":[],"value":3,"fixed":true},{"r":8,"c":6,"numbers":[4,5]},{"r":8,"c":7,"numbers":[4,6]},{"r":8,"c":8,"numbers":[],"value":8},{"r":8,"c":9,"numbers":[],"value":2,"fixed":true},{"r":9,"c":1,"numbers":[],"value":8,"fixed":true},{"r":9,"c":2,"numbers":[],"value":3},{"r":9,"c":3,"numbers":[5,6]},{"r":9,"c":4,"numbers":[4,5]},{"r":9,"c":5,"numbers":[],"value":2},{"r":9,"c":6,"numbers":[],"value":1,"fixed":true},{"r":9,"c":7,"numbers":[],"value":9,"fixed":true},{"r":9,"c":8,"numbers":[4,6]},{"r":9,"c":9,"numbers":[],"value":7,"fixed":true}]';
-    this.matrix = JSON.parse(mockString); 
   }
 
 }
